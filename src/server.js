bbab4446dee2ca683f4fa6b94698c7bd9ca9b19b
henriqueganz <henrique.ganz@gmail.com>
2023-05-23 15:08:08 -0300

    validacao de email

--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ const AppError= require("./utils/AppError");
 const express = require("express");
 
 const routes = require("./routes");
+const migrationsRun = require("./database/sqlite/migrations");
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+migrationsRun();
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
